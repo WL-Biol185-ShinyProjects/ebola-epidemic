3d9d4178800e1a20876b22902c1e3cf9dcae4857
Caroline <burkec18@mail.wlu.edu>
2017-02-16 12:53:29 -0500

    test

---
+++ ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+sliderInput("slider2", label = h3("Slider Range"), min = 0, 
+            max = 100, value = c(40, 60))
