7fd85e279b6931d09c2b1d342805d54ef1aad89e
Patrick <murphyp18@mail.wlu.edu>
2017-02-28 13:00:41 -0500

    Test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ebola-epidemic
 Using Ebola epidemic data to dynamically model spread. Will hopefully integrate cultural data.
 
-"whats up caroline"
+hi
---
+++ server.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+
+shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+   
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+  function(input, output) {
+    
+    output$value <- renderPrint({ input$action })
+    
+  }
+  
+})
