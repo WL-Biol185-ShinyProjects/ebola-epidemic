77cc4336025606567a26e1d7be4cf82de2b4804d
Patrick <murphyp18@mail.wlu.edu>
2017-04-01 16:36:46 -0400

    Many Changes have occured

--- server.R
+++ server.R
@@ -1,23 +1,130 @@
 library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(leaflet)
 
 
-shinyServer(function(input, output) {
+function(input, output) {
+  datavariable <- read.table("datafordeathfile")
   
-  output$distPlot <- renderPlot({
+  countrydata <- read.table("c_w_nationalfile")
+  
+  
+  datavariable$NewDate <- as.Date(as.character(datavariable$NewDate))
+  datavariable$Localite <- as.character(datavariable$Localite)
+  datavariable$Value <- as.numeric(datavariable$Value)
+  
+  
+  output$Map <- renderLeaflet({
+
+    #print(datavariable %>%  filter(NewDate == as.Date(input$Dateslider)))
+    #print(as.Date(input$Dateslider))
+    #print(class(datavariable$Localite))
+    #print(filter(datavariable, as.numeric(NewDate) == as.numeric(input$Dateslider)))
+    #class(datavariable$numericvalue)
     
+     datavariable %>% 
+      
+      
+     filter(NewDate == min(datafordeath$NewDate)) %>%
+       
+      
+ 
+      leaflet() %>%
+      setView(lng = -9.311828, lat = 10.38279, zoom = 4) %>%
+      addTiles() %>%
+      addCircles(radius = ~Value*150, 
+                       label = ~Localite, 
+                       fillOpacity = .05)
+        
+  })
+  
+  
+  
+  observe({
     
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   if(input$Dateslider %in% datavariable$NewDate) {
+  
+     
+     
+    datavariable %>% 
+       
+    filter(NewDate == as.Date(input$Dateslider)) %>%
     
-   
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    leafletProxy("Map", data=.) %>% 
+    
+    clearShapes() %>%
+       
+    addCircles(radius = ~Value*150, 
+              label = ~Localite, 
+              fillOpacity = .05) 
+ 
+     
+   }
     
   })
   
-  function(input, output) {
-    
-    output$value <- renderPrint({ input$action })
-    
-  }
   
-})
+  
+  
+  
+  
+ ########################################### 
+  
+  output$death_graph <- output$death_graph <- renderPlot({
+    
+   
+      datavariable %>% 
+      
+      
+      filter(NewDate == as.Date(input$Dateslider)) %>%
+     
+    
+    
+   
+    ggplot(aes(Localite, Value)) + geom_point(aes(colour = Localite)) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+  })
+ 
+
+ ############################### 
+  
+  
+  output$country_graph <- renderPlot({
+    
+    
+    countrydata %>% 
+      
+    
+    ggplot(aes(NewDate, Numericvalue)) + geom_point(aes(colour=Country)) + theme(axis.text.x = element_text(size = 6, angle = 60, hjust = 1))
+      
+      
+      
+      
+  })
+######################################################  
+  
+
+
+  output$GDPgraph <- renderPlot ({
+    
+    countrydata %>% 
+      
+      ggplot(aes(input$selectX, input$selectY)) + geom_point(aes(size=GDP, colour=Country))
+    
+    
+    })
+    
+    
+ ######################################################## 
+  
+  
+  
+  
+  
+
+
+}
+
+
+
+  
--- ui.R
+++ ui.R
@@ -1,34 +1,100 @@
 library(shiny)
+library(shinydashboard)
 
 
-fluidPage(
+
+dashboardPage(
   
- 
-  titlePanel("Old Faithful Geyser Data"),
-
   
-
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+  dashboardHeader(title = "Basic dashboard"),
+  
+  
+  
+  dashboardSidebar(sidebarMenu(
+   
+     menuItem("Localite", tabName = "dashboard"),
     
-
-    actionButton("do", 
-                 "Click Me"),
+     menuItem("Country", tabName = "Country"))),
+  
+  
+  
+  
+  
+  
+  
+  dashboardBody(
+    
+    tabItems(
+      
+      tabItem("dashboard",
+    
+              
+              
+              fluidRow(
+                 box(background = "black", leafletOutput("Map", height = 250)),
+                 
+                 box(
+                   title = "Controls", background = "black",
+                   #sliderInput("Dateslider", "Date:", 
+                   #min = min(datafordeath$NewDate), 
+                   #max = max(datafordeath$NewDate), 
+                   #value = min(datafordeath$NewDate))
+                   sliderInput("Dateslider", "Looping Animation:", 
+                               min(datafordeath$NewDate), 
+                               max(datafordeath$NewDate), 
+                               min(datafordeath$NewDate),
+                               step = 1, 
+                               animate=
+                                 animationOptions(interval=300, loop=TRUE)))),
+               
+               
+              fluidRow(
+                 box(plotOutput("death_graph", height = "400", width = "1000")))),
     
     
-    mainPanel(
-      plotOutput("distPlot")
-    )
+    
+    
+      tabItem("Country",
+            
+              
+              fluidRow(
+              box(plotOutput("country_graph", height = "400", width = "1000"))),
+                
+                
+              fluidRow(
+              #box(selectInput("selectX", label = h3("Select box for X"), 
+                              #choices = c("Household Size" = c_w_national$Household_Size, "Percent Urban" = c_w_national$Percent_Urban,  "Percent with Internet" = c_w_national$Percent_using_Internet), 
+                              #selected = "Household Size"),
+                  
+              #box(selectInput("selectY", label = h3("Select box for Y"), 
+                              #choices = c("Household Size" = c_w_national$Household_Size, "Percent Urban" = c_w_national$Percent_Urban,  "Percent with Internet" = c_w_national$Percent_using_Internet), 
+                              #selected = "Household Size"),
+              
+              #plotOutput("GDPgraph", height = "400", width = "600")))
+                
+                
+              box(selectInput("selectX", label = h3("Select box for X"), 
+                              choices = c("Household Size" = c_w_national$Household_Size, 
+                                          "Percent Urban" = c_w_national$Percent_Urban,  
+                                          "Percent with Internet" = c_w_national$Percent_using_Internet), 
+                              selected = "Household Size"),
+                  
+              box(selectInput("selectY", label = h3("Select box for Y"), 
+                              choices = c("Household Size" = c_w_national$Household_Size, 
+                                          "Percent Urban" = c_w_national$Percent_Urban,  
+                                          "Percent with Internet" = c_w_national$Percent_using_Internet), 
+                              selected = "Household Size"),
+                      
+              plotOutput("GDPgraph", height = "400", width = "600")))
+              
+              
+                
   )
-)
+    ))))
+      
+          
 
-sliderInput("slider2", label = h3("Slider Range"), min = 0, 
-            max = 100, value = c(40, 60))
+
+
 
 
