214437b94b7aa0f521463d0a520a1e811ad6bedb
Patrick <murphyp18@mail.wlu.edu>
2017-04-06 13:15:03 -0400

    finalll

--- ui.R
+++ ui.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
-
+library(dplyr)
+library(leaflet)
+library(scales)
 #####################################################
 datavariable <- read.table("datafordeathfile")
 
